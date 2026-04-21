5330f89b5abe20717f6f3ed6bf712bf852073d69
Pablo Elías Avendaño Miranda <pablo.elias.miranda.292003@gmail.com>
2026-04-21 13:08:23 -0400

    build: reestructuracin de entorno y limpieza de raz (pre-migracin 1.21.11)

---
+++ README.md
@@ -0,0 +1,18 @@
+# SFCalc
+A calculator for the plugin Slimefun. This is based off of john000708's SlimeCalculator.
+
+Find most recent build [here](https://blob.build/project/SFCalc/Dev).
+
+## Usage
+
+This addon for Slimefun adds a new command: /sfcalc. This command can calculate how much basic slimefun resources you need to make the specified item.
+
+Usage: `/sfcalc [required: item] [optional: amount]`
+
+Item is the id of the item you want to craft. An item id is like this: "electric_motor", "carbonado", "solar_generator" (basic solar generator), etc. For tiered machines that are not lowest tier, add the tier number after the id. For example: "solar_generator_3" (carbonado solar generator), "lava_generator_2" (advanced lava generator), "carbon_press" (carbon press [tier 1]), etc.
+
+Amount is the number of items you want to craft. It defaults to 1 if you don't specify any amount.
+
+Minecraft version: 1.16+
+
+Slimefun version: RC-36 or newer
---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    java
+    id("com.gradleup.shadow") version "8.3.2"
+}
+
+repositories {
+    mavenLocal()
+    maven("https://jitpack.io/")
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://repo.maven.apache.org/maven2/")
+}
+
+dependencies {
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
+    compileOnly("com.github.Slimefun:Slimefun4:RC-37")
+
+    implementation("org.bstats:bstats-bukkit:3.0.2")
+    implementation("com.github.seggan:ErrorReporter-Java:1.1.0")
+
+    compileOnly("com.google.code.findbugs:jsr305:3.0.2")
+    compileOnly("org.projectlombok:lombok:1.18.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
+}
+
+group = "io.github.seggan.sfcalc"
+version = "UNOFFICIAL"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("")
+
+    relocate("io.github.seggan.errorreporter", "io.github.seggan.sfcalc.errorreporter")
+    relocate("org.bstats", "io.github.seggan.sfcalc.bstats")
+}
